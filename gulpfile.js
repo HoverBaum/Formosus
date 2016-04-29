a19ec41a17889f159261e508781eb707779fe14d
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-04-29 16:59:39 +0800

    Make gulp task better
    strip comments from js and minify html.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 var gulp = require('gulp');
 var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var minifyCss = require('gulp-minify-css');
+var strip = require('gulp-strip-comments');
+var htmlmin = require('gulp-htmlmin');
 
-gulp.task('html', function() {
-	var assets = useref.assets();
+/*
+	A word on uglifying JS
+	We should do that but currently all uglifiers have problems with ES6 syntax and we don't want to add babel to our dev stack.
+	So for now no minified or uglified JS.
 
-	gulp.src('index.html')
-		.pipe(assets)
-		.pipe(gulpif('*.css', rebaseUrls()))
-		.pipe(gulpif('*.css', minifyCss()))
-		.pipe(assets.restore())
-		.pipe(useref())
-		.pipe(minifyInline())
-		.pipe(minifyHtml())
-		.pipe(gulp.dest('./dist'));
-});
+	At least we remove comments...
+ */
 
 gulp.task('default', function () {
     return gulp.src('index.html')
         .pipe(useref())
+		.pipe(gulpif('*.css', minifyCss()))
+		.pipe(gulpif('*.js', strip()))
+		.pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
         .pipe(gulp.dest('dist'));
 });
