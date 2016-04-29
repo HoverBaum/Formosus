fa843ecf17b8ea1cc7f40015277bfa58266ad80f
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-04-29 17:37:24 +0800

    Extend gulp workflow to create .zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-var gulp = require('gulp');
-var useref = require('gulp-useref');
-var gulpif = require('gulp-if');
-var minifyCss = require('gulp-minify-css');
-var strip = require('gulp-strip-comments');
-var htmlmin = require('gulp-htmlmin');
+const gulp = require('gulp');
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+const minifyCss = require('gulp-minify-css');
+const strip = require('gulp-strip-comments');
+const htmlmin = require('gulp-htmlmin');
+const zip = require('gulp-zip');
 
 /*
 	A word on uglifying JS
@@ -21,11 +22,17 @@ gulp.task('assets', function() {
 	.pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['assets'], function () {
-    gulp.src('index.html')
+gulp.task('html', function() {
+	gulp.src('index.html')
         .pipe(useref())
 		.pipe(gulpif('*.css', minifyCss()))
 		.pipe(gulpif('*.js', strip()))
 		.pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
         .pipe(gulp.dest('dist'));
+})
+
+gulp.task('default', ['assets', 'html'], function () {
+	gulp.src('dist/**/*')
+	.pipe(zip('formosus.zip'))
+	.pipe(gulp.dest('./'));
 });
