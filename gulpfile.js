31f60bff5cf6bab24236d716c082d4130553bc9b
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-05-23 12:42:07 +0800

    Updated build workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const minifyCss = require('gulp-minify-css');
 const strip = require('gulp-strip-comments');
 const htmlmin = require('gulp-htmlmin');
 const zip = require('gulp-zip');
+const concat = require('gulp-concat');
+const del = require('del');
+const runSequence = require('run-sequence');
 
 /*
 	A word on uglifying JS
@@ -18,10 +21,11 @@ const zip = require('gulp-zip');
  *   Copy over files that need no handling.
  */
 gulp.task('assets', function() {
-	gulp.src(['manifest.json', 'fonts/**/*', 'img/**/*'],{base: './'})
+	gulp.src(['fonts/**/*', 'img/**/*'],{base: './'})
 	.pipe(gulp.dest('dist/'));
 });
 
+//Handle the frontend
 gulp.task('html', function() {
 	gulp.src('index.html')
         .pipe(useref())
@@ -29,10 +33,36 @@ gulp.task('html', function() {
 		.pipe(gulpif('*.js', strip()))
 		.pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
         .pipe(gulp.dest('dist'));
-})
+});
 
-gulp.task('default', ['assets', 'html'], function () {
+//Update and save the manifest.
+gulp.task('manifest', function() {
+	var fs = require('fs');
+	var path = require('path');
+	var manifest = require('./manifest');
+	manifest.background.scripts = ['backgroundScript.js'];
+	fs.writeFile(path.join('dist', 'manifest.json'), JSON.stringify(manifest));
+});
+
+//Handle the background scripts.
+gulp.task('background', function() {
+	gulp.src('js/background/*.js')
+	.pipe(concat('backgroundScript.js'))
+	.pipe(strip())
+	.pipe(gulp.dest('dist'))
+});
+
+//Delte old files.
+gulp.task('clean', function() {
+	return del(['dist/**/*']);
+});
+
+gulp.task('dist', ['assets', 'html', 'manifest', 'background'], function() {
 	gulp.src('dist/**/*')
 	.pipe(zip('formosus.zip'))
 	.pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['clean'], function () {
+	runSequence('dist');
 });
